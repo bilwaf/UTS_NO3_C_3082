2954768f111cb7a10b6e12db65274f6f0e71f8a7
bilwaf <116044468+bilwaf@users.noreply.github.com>
2022-12-11 13:46:32 +0700

    Add files via upload

--- UTS_NO3_C_3082/src/main/java/com/mycompany/uts_no3_c_3082/UTS_NO3_C_3082.java
+++ UTS_NO3_C_3082/src/main/java/com/mycompany/uts_no3_c_3082/UTS_NO3_C_3082.java
@@ -15,12 +15,28 @@ package com.mycompany.uts_no3_c_3082;
 public class UTS_NO3_C_3082 {
 
     public static void main(String[] args) {
-        AsistenPraktikum_3082 asprak = new AsistenPraktikum_3082("PBO", 5, "20103041", "Langit", "Sistem Informasi", 4.0);
-        asprak.totalPendapatan_3082();
-        asprak.tampilDataMhs_3082();
+        AsistenPraktikum_3082 asprak1 = new AsistenPraktikum_3082("PBO", 5, "20100041", "Langit", "Sistem Informasi", 4.0);
+        AsistenPraktikum_3082 asprak2 = new AsistenPraktikum_3082("Algoritme Pemrograman", 3, "20100000", "Harsa", "Teknik Informatika", 3.89);
+        AsistenPraktikum_3082 asprak3 = new AsistenPraktikum_3082("Basis Data", 4, "20101000", "Jemari", "Sistem Informasi", 3.91);
+        asprak1.totalPendapatan_3082();
+        asprak1.tampilDataMhs_3082();
+        asprak2.totalPendapatan_3082();
+        asprak2.tampilDataMhs_3082();
+        asprak3.totalPendapatan_3082();
+        asprak3.tampilDataMhs_3082();
         
-        StudentStaff_3082 staff = new StudentStaff_3082(1, 5, "21103200", "Neraca", "Sistem Informasi", 4.0);
-        staff.totalPendapatan_3082();
-        staff.tampilDataMhs_3082();
+        System.out.println("");
         
-}}
+        StudentStaff_3082 staff1 = new StudentStaff_3082(1, 5, "21100080", "Nona", "Sistem Informasi", 4.0);
+        StudentStaff_3082 staff2 = new StudentStaff_3082(1, 3, "21100002", "Insan", "RPL", 4.0);
+        StudentStaff_3082 staff3 = new StudentStaff_3082(1, 2, "21101000", "Hiranya", "Teknik Informatika", 3.78);
+        staff1.totalPendapatan_3082();
+        staff1.tampilDataMhs_3082();
+        staff2.totalPendapatan_3082();
+        staff2.tampilDataMhs_3082();
+        staff3.totalPendapatan_3082();
+        staff3.tampilDataMhs_3082();
+        
+        
+    }
+}
